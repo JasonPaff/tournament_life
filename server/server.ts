c83ec4fa0b336d1778d459cfedc12890e6b0c02d
Jason <jasonpaff@gmail.com>
2023-03-11 15:21:26 -0500

    client/server setup

--- server/server.ts
+++ server/server.ts
@@ -22,7 +22,7 @@ server.use(logger('dev', {}));
 server.use(express.json());
 
 // Serve the React static files after build
-server.use(express.static('../client/dist'));
+//server.use(express.static(''));
 
 server.listen(PORT, () => {
     console.log('server started');
